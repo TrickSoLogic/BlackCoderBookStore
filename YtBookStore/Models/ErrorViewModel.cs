cacecaa63a2585568378af40a24afc059e4d6d49
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-01-19 23:29:51 +0530

    Add project files.

---
+++ YtBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlackCoderBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
